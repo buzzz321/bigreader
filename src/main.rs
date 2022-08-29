d6eeb2817720463086e46335bd41edb3f140d619
Anders Olme <anders.olme@gmail.com>
2022-08-29 21:13:26 +0200

    Added text window
    Now with slicing we can get to lines within the text buffer.
    We still dont take care of case when last window has less rows than
    wanted rows.

--- src/main.rs
+++ src/main.rs
@@ -24,16 +24,45 @@ fn open_file(filename: String) -> String {
     }
 }
 
+fn get_text_window(curr_pos: usize, rows: usize, buffer: &String) -> Option<(usize, &str)> {
+    let bytebuff = &buffer.as_bytes()[curr_pos..];
+    let mut lines: usize = 0;
+    let mut pos = curr_pos;
+    for char in bytebuff.bytes() {
+        if char.unwrap() == '\n' as u8 {
+            lines += 1;
+        }
+        pos += 1;
+        if lines >= rows {
+            return Some((curr_pos + pos, &buffer[curr_pos..pos]));
+        }
+    }
+    return None;
+}
+
 fn main() {
     let filename = "data.txt";
     let res = open_file(filename.to_string());
+
+    let new_lines = get_text_window(78888890, 5, &res);
+    match new_lines {
+        Some((new_pos, myslice)) => println!("new_pos {} \nslice: \n{}", new_pos, myslice),
+        None => println!("could get more windows"),
+    };
+
     let mut lines: u64 = 0;
+    let mut pos: u64 = 0;
     for char in res.bytes() {
         // guess this breaks in utf8..
         if char == '\n' as u8 {
             lines += 1;
         }
+        pos += 1;
+
+        if lines == 8000000{
+            println!("pos {}", pos);
+        }
     }
 
-    println!("Hello, world! {}", lines);
+    println!("Lines in file {}", lines);
 }
